620459c05e4167e7a79f8b57b59175d675483510
zccz14 <zccz14@outlook.com>
2017-07-28 19:34:14 +0800

    ZigZag

--- QuantX/TI/ZigZag.cs
+++ QuantX/TI/ZigZag.cs
@@ -5,22 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace QuantX.TI {
-	public struct ZigZagData {
-		public double value;
-		/// <summary>
-		/// isHigh
-		/// </summary>
-		public bool isHigh, isLow;
-		public ZigZagData (double v, bool h, bool l) {
+	public class ZigZagData {
+		public double value, high, low;
+		public ZigZagData (double v, double h, double l) {
 			value = v;
-			isHigh = h;
-			isLow = l;
+			high = h;
+			low = l;
 		}
 	}
 	public struct ZigZagParam {
 		public ITI<double> high, low;
 		public int depth, deviation, backstep;
-		public ZigZagParam(ITI<double> h, ITI<double> l, int dep, int dev, int bs) {
+		public ZigZagParam (ITI<double> h, ITI<double> l, int dep, int dev, int bs) {
 			high = h;
 			low = l;
 			depth = dep;
@@ -29,11 +25,7 @@ namespace QuantX.TI {
 		}
 	}
 	public class ZigZag : ITI<ZigZagData> {
-		public IReadOnlyList<ZigZagData> History {
-			get {
-				return bufHistory;
-			}
-		}
+		public IReadOnlyList<ZigZagData> History => throw new Exception("Not Implemented");
 
 		public event EventHandler<ZigZagData> OnData;
 		/// <summary>
@@ -62,140 +54,247 @@ namespace QuantX.TI {
 			param.low.OnData += main;
 		}
 		private void main (object sender, double value) {
-			int cc = Math.Min(arg.high.History.Count, arg.high.History.Count);
-			for (int i = bufHistory.Count; i < cc; i++) {
-				while (bufHistory.Count < i) {
-					bufHistory.Add(new ZigZagData());
-				}
-				int res = calc(i, prev, arg.high.History, arg.low.History);
-				for (int j = prev; j < res; j++) {
-					OnData?.Invoke(this, bufHistory[j]);
-				}
-				prev = res;
+			int cc = Math.Min(arg.high.History.Count, arg.low.History.Count);
+			while (ZigzagBuffer.Count < cc) {
+				ZigzagBuffer.Add(0);
+				HighMapBuffer.Add(0);
+				LowMapBuffer.Add(0);
+			}
+			int res = calc(cc, prev, arg.high.History, arg.low.History);
+			OnData?.Invoke(this, null);
+			prev = res;
+		}
+		private void ArrayInitialize<T> (List<T> list, T value) {
+			for (int i = 0; i < list.Count; i++) {
+				list[i] = value;
 			}
 		}
-		enum State {
-			BOTH = 0,
-			HIGH = 1,
-			LOW = -1
-		};
+		private bool IsStopped () {
+			return false;
+		}
+		public List<double> LowMapBuffer = new List<double>();
+		public List<double> HighMapBuffer = new List<double>();
+		public List<double> ZigzagBuffer = new List<double>();
+		private int ExtDepth => arg.depth;
+		private double deviation => arg.deviation;
+		private int ExtBackstep => arg.backstep;
+		private const int prepare = 200;
+		private const int level = 3;
+		private const int Pike = 1;
+		private const int Sill = -1;
 		private int calc (int rates_total, int prev_calculated, IReadOnlyList<double> high, IReadOnlyList<double> low) {
-			int determinted = prev_calculated;
+			int i = 0;
+			int limit = 0, counterZ = 0, whatlookfor = 0;
+			int shift = 0, back = 0, lasthighpos = 0, lastlowpos = 0;
+			double val = 0, res = 0;
+			double curlow = 0, curhigh = 0, lasthigh = 0, lastlow = 0;
+			//--- initializing
 			if (prev_calculated == 0) {
+				ArrayInitialize(ZigzagBuffer, 0.0);
 
+				ArrayInitialize(HighMapBuffer, 0.0);
+
+				ArrayInitialize(LowMapBuffer, 0.0);
 			}
-			double lastlow, lasthigh;
-			int lastlowpos, lasthighpos;
-			State s = State.BOTH;
-			ZigZagData cur;
-			if (prev_calculated == 0) {
-				s = State.BOTH;
-				lasthigh = 0;
-				lastlow = 0;
-				lasthighpos = -1;
-				lastlowpos = -1;
-			} else {
-				if (bufHistory[prev_calculated - 1].isHigh) {
-					s = State.LOW;
-					lasthigh = bufHistory[prev_calculated - 1].value;
-					lasthighpos = prev_calculated - 1;
-					lastlow = 0;
-					lastlowpos = -1;
-				} else if (bufHistory[prev_calculated - 1].isLow) {
-					s = State.HIGH;
-					lasthigh = 0;
-					lasthighpos = -1;
-					lastlow = bufHistory[prev_calculated - 1].value;
-					lastlowpos = prev_calculated - 1;
+			//--- 
+			if (rates_total < prepare)
+				return (0);
+			//--- set start position for calculations
+			if (prev_calculated == 0)
+				limit = ExtDepth;
+
+			//--- ZigZag was already counted before
+			if (prev_calculated > 0) {
+				i = rates_total - 1;
+				//--- searching third extremum from the last uncompleted bar
+				while (counterZ < level && i > rates_total - prepare) {
+					res = ZigzagBuffer[i];
+					if (res != 0)
+						counterZ++;
+					i--;
+				}
+				i++;
+				limit = i;
+
+				//--- what type of exremum we are going to find
+				if (LowMapBuffer[i] != 0) {
+					curlow = LowMapBuffer[i];
+					whatlookfor = Pike;
 				} else {
-					throw new Exception("ZigZag[prev_calculated - 1] is not a high/low point");
+					curhigh = HighMapBuffer[i];
+					whatlookfor = Sill;
+				}
+				//--- chipping
+				for (i = limit + 1; i < rates_total && !IsStopped(); i++) {
+					ZigzagBuffer[i] = 0.0;
+					LowMapBuffer[i] = 0.0;
+					HighMapBuffer[i] = 0.0;
 				}
 			}
-			// launch
-			for (int i = prev_calculated; i < rates_total; i++) {
-				// get low
-				{
-					double nearlow = low.ReverseAt(i).Take(arg.depth).Min();
-					if (lastlow != nearlow) {
-						lastlow = nearlow;
-						if (low[i] <= nearlow + arg.deviation) {
-							for (int j = 0; j < arg.backstep; j++) {
-								if (i - j - 1 >= 0 && bufHistory[i - j - 1].isLow && bufHistory[i - j - 1].value > nearlow) {
-									bufHistory[i - j - 1] = new ZigZagData();
-								}
-							}
+
+			//--- searching High and Low
+			for (shift = limit; shift < rates_total && !IsStopped(); shift++) {
+				val = low[iLowest(low, ExtDepth, shift)];
+				if (val == lastlow)
+					val = 0.0;
+				else {
+					lastlow = val;
+					if ((low[shift] - val) > deviation)
+						val = 0.0;
+					else {
+						for (back = 1; back <= ExtBackstep; back++) {
+							res = LowMapBuffer[shift - back];
+							if ((res != 0) && (res > val))
+								LowMapBuffer[shift - back] = 0.0;
 						}
-					}
-					if (low[i] == nearlow) {
-						bufHistory[i] = new ZigZagData(low[i], false, true);
-					} else {
-						bufHistory[i] = new ZigZagData();
 					}
 				}
-				// get high
-				{
-					double nearhigh = high.ReverseAt(i).Take(arg.depth).Max();
-					if (lasthigh != nearhigh) {
-						lasthigh = nearhigh;
-						if (high[i] >= nearhigh - arg.deviation) {
-							for (int j = 0; j < arg.backstep; j++) {
-								if (i - j - 1 >= 0 && bufHistory[i - j - 1].isHigh && bufHistory[i - j - 1].value < nearhigh) {
-									bufHistory[i - j - 1] = new ZigZagData();
-								}
-							}
+				if (low[shift] == val)
+					LowMapBuffer[shift] = val;
+				else
+					LowMapBuffer[shift] = 0.0;
+				//--- high
+				val = high[iHighest(high, ExtDepth, shift)];
+				if (val == lasthigh)
+					val = 0.0;
+				else {
+					lasthigh = val;
+					if ((val - high[shift]) > deviation)
+						val = 0.0;
+					else {
+						for (back = 1; back <= ExtBackstep; back++) {
+							res = HighMapBuffer[shift - back];
+							if ((res != 0) && (res < val))
+								HighMapBuffer[shift - back] = 0.0;
 						}
-					}
-					if (high[i] == nearhigh) {
-						bufHistory[i] = new ZigZagData(high[i], true, false);
-					} else {
-						bufHistory[i] = new ZigZagData();
 					}
 				}
-				{
-					if (s == State.BOTH) {
-						if (lasthigh == 0 && lastlow == 0) {
-							if (bufHistory[i].isHigh) {
-								lasthigh = high[i];
-								lasthighpos = i;
-								s = State.LOW;
+				if (high[shift] == val)
+					HighMapBuffer[shift] = val;
+				else
+					HighMapBuffer[shift] = 0.0;
+			}
+
+			//--- last preparation
+			if (whatlookfor == 0)// uncertain quantity
+			  {
+				lastlow = 0;
+				lasthigh = 0;
+			} else {
+				lastlow = curlow;
+				lasthigh = curhigh;
+			}
+
+			//--- final rejection
+			for (shift = limit; shift < rates_total && !IsStopped(); shift++) {
+				switch (whatlookfor) {
+					case 0: // search for peak or lawn
+						if (lastlow == 0 && lasthigh == 0) {
+							if (HighMapBuffer[shift] != 0) {
+								lasthigh = high[shift];
+								lasthighpos = shift;
+								whatlookfor = Sill;
+								ZigzagBuffer[shift] = lasthigh;
 							}
-							if (bufHistory[i].isLow) {
-								lastlow = low[i];
-								lastlowpos = i;
-								s = State.HIGH;
+							if (LowMapBuffer[shift] != 0) {
+								lastlow = low[shift];
+								lastlowpos = shift;
+								whatlookfor = Pike;
+								ZigzagBuffer[shift] = lastlow;
 							}
 						}
-					} else if (s == State.HIGH) {
-						if (bufHistory[i].isLow && bufHistory[i].value < lastlow) {
-							bufHistory[lastlowpos] = new ZigZagData();
-							lastlowpos = i;
-							lastlow = bufHistory[i].value;
+						break;
+					case Pike: // search for peak
+						if (LowMapBuffer[shift] != 0.0 && LowMapBuffer[shift] < lastlow && HighMapBuffer[shift] == 0.0) {
+							ZigzagBuffer[lastlowpos] = 0.0;
+							lastlowpos = shift;
+							lastlow = LowMapBuffer[shift];
+							ZigzagBuffer[shift] = lastlow;
 						}
-						if (bufHistory[i].isHigh) {
-							lasthighpos = i;
-							lasthigh = bufHistory[i].value;
-							s = State.LOW;
+						if (HighMapBuffer[shift] != 0.0 && LowMapBuffer[shift] == 0.0) {
+							lasthigh = HighMapBuffer[shift];
+							lasthighpos = shift;
+							ZigzagBuffer[shift] = lasthigh;
+							whatlookfor = Sill;
 						}
-					} else if (s == State.LOW) {
-						if (bufHistory[i].isHigh && bufHistory[i].value > lasthigh) {
-							bufHistory[lasthighpos] = new ZigZagData();
-							lasthighpos = i;
-							lasthigh = bufHistory[i].value;
+						break;
+					case Sill: // search for lawn
+						if (HighMapBuffer[shift] != 0.0 && HighMapBuffer[shift] > lasthigh && LowMapBuffer[shift] == 0.0) {
+							ZigzagBuffer[lasthighpos] = 0.0;
+							lasthighpos = shift;
+							lasthigh = HighMapBuffer[shift];
+							ZigzagBuffer[shift] = lasthigh;
 						}
-						if (bufHistory[i].isLow) {
-							lastlowpos = i;
-							lasthigh = bufHistory[i].value;
-							s = State.HIGH;
+						if (LowMapBuffer[shift] != 0.0 && HighMapBuffer[shift] == 0.0) {
+							lastlow = LowMapBuffer[shift];
+							lastlowpos = shift;
+							ZigzagBuffer[shift] = lastlow;
+							whatlookfor = Pike;
 						}
-					} else {
-						throw new Exception("Invalid ZigZag State: " + s);
-					}
+						break;
+					default:
+						return (rates_total);
 				}
 			}
-			return determinted;
+
+			//--- return value of prev_calculated for next call
+			return (rates_total);
+		}
+		//+------------------------------------------------------------------+
+		//|  searching index of the highest bar                              |
+		//+------------------------------------------------------------------+
+		private int iHighest (IReadOnlyList<double> array,
+			 int depth,
+
+			 int startPos) {
+			int index = startPos;
+			//--- start index validation
+			if (startPos < 0) {
+
+				return 0;
+			}
+			int size = array.Count;
+			//--- depth correction if need
+			if (startPos - depth < 0)
+				depth = startPos;
+			double max = array[startPos];
+			//--- start searching
+			for (int i = startPos; i > startPos - depth; i--) {
+				if (array[i] > max) {
+					index = i;
+					max = array[i];
+				}
+			}
+			//--- return index of the highest bar
+			return (index);
+		}
+		//+------------------------------------------------------------------+
+		//|  searching index of the lowest bar                               |
+		//+------------------------------------------------------------------+
+		int iLowest (IReadOnlyList<double> array,
+					int depth,
+					int startPos) {
+			int index = startPos;
+			//--- start index validation
+			if (startPos < 0) {
+				return 0;
+			}
+			int size = array.Count;
+			//--- depth correction if need
+			if (startPos - depth < 0)
+				depth = startPos;
+			double min = array[startPos];
+			//--- start searching
+			for (int i = startPos; i > startPos - depth; i--) {
+				if (array[i] < min) {
+					index = i;
+					min = array[i];
+				}
+			}
+			//--- return index of the lowest bar
+			return (index);
 		}
 		private ZigZagParam arg;
-		private List<ZigZagData> bufHistory = new List<ZigZagData>();
 		private int prev = 0;
 	}
 }
